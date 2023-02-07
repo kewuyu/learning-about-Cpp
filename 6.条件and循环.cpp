5fcee9ad955f7eab2477cbfe9beda2a3ef1ea3e1
可无语 <109637491+kewuyu@users.noreply.github.com>
2023-02-07 10:40:51 +0800

    Create 6.条件and循环.cpp

---
+++ 6.条件and循环.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+int main()
+{
+  int a =6;
+  if(a==6)
+  {
+    for(int i = 1;i<=a;i++)
+    {
+      std::cout<<"Hello Word"<<std::endl;
+    }
+  }
+  else
+  {
+    std::cout<<a<<std::endl;
+  }
+  
+  return 0;
+}
