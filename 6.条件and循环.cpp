c46cac9ddb43e4cc1081a60cce9183b99b28bf1b
可无语 <109637491+kewuyu@users.noreply.github.com>
2023-02-07 10:46:16 +0800

    Update 6.条件and循环.cpp

--- 6.条件and循环.cpp
+++ 6.条件and循环.cpp
@@ -2,18 +2,24 @@
 
 int main()
 {
-  int a =6;
-  if(a==6)
-  {
-    for(int i = 1;i<=a;i++)
+    int a = 6;
+    int i;
+    if (a == 6)
     {
-      std::cout<<"Hello Word"<<std::endl;
+        for ( i = 1; i <= a; i++)
+        {
+            std::cout << "Hello Word" << std::endl;
+        }
     }
-  }
-  else
-  {
-    std::cout<<a<<std::endl;
-  }
-  
-  return 0;
+    else
+    {
+        std::cout << a << std::endl;
+    }
+
+    while (i <= 7)
+    {
+        std::cout << "true" << std::endl;
+        i++;
+    }
+    return 0;
 }
